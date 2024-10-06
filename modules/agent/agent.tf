b339fa414023d8eab5d30bb26baf8dcfaedf5a52
tosi29 <tosi16@gmail.com>
2024-10-07 01:13:56 +0900

    モデルの指定を変更

--- modules/agent/agent.tf
+++ modules/agent/agent.tf
@@ -1,7 +1,7 @@
 resource "aws_bedrockagent_agent" "this" {
   agent_name              = "${var.prefix}-agent"
   agent_resource_role_arn = aws_iam_role.agents.arn
-  foundation_model        = "anthropic.claude-3-5-sonnet-20240620-v1:0"
+  foundation_model        = data.aws_bedrock_foundation_model.haiku.model_id
   prepare_agent           = true
   instruction             = "あなたは仕事のアシスタントです。必要に応じてナレッジベースを参照したり、コードを書いたりして仕事の手助けをしてください。"
 }
